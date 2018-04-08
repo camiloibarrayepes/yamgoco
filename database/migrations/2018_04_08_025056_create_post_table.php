fb29a74646d8f2a8e9424475bcfbb233c1cf2176
camilo ibarra <camilo.ibarray@gmail.com>
2018-04-07 22:02:57 -0500

    Design in project

---
+++ database/migrations/2018_04_08_025056_create_post_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('post', function (Blueprint $table) 
+        {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('author_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('post', function (Blueprint $table) 
+        {
+            Schema::drop('posts');
+        });
+    }
+}
