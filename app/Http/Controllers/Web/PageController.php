17ab33024f786434bd640ace9e2f387438f231d7
camilo ibarra <camilo.ibarray@gmail.com>
2018-04-16 19:29:25 -0500

    category filter

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -6,11 +6,20 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Post;
+use App\Category;
 
 class PageController extends Controller
 {
     public function blog(){
         $posts = Post::orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+        return view('web.posts', compact('posts'));
+    }
+
+    public function category($slug){
+        $category = Category::where('slug', $slug)->pluck('id')->first();
+        $posts = Post::where('category_id', $category)
+            ->orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+
         return view('web.posts', compact('posts'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Auth::routes();
 
 Route::get('/blog', 'Web\PageController@blog')->name('blog');
 
-Route::get('blog/{slug}', 'web\PageController@post')->name('post');
+Route::get('entrada/{slug}', 'web\PageController@post')->name('post');
+Route::get('category/{slug}', 'web\PageController@category')->name('category');
+Route::get('etiqueta/{slug}', 'web\PageController@tag')->name('tag');
