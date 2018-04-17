ef47dcda984898afa5c57b5d4f7bfced8cb56713
camilo ibarra <camilo.ibarray@gmail.com>
2018-04-16 19:34:42 -0500

    slug filter

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -23,6 +23,16 @@ class PageController extends Controller
         return view('web.posts', compact('posts'));
     }
 
+    public function tag($slug){
+        
+        $posts = Post::whereHas('tags', function($query) use ($slug){
+            $query->where('slug', $slug);
+        })
+        ->orderBy('id', 'DESC')->where('status', 'PUBLISHED')->paginate(3);
+
+        return view('web.posts', compact('posts'));
+    }
+
     public function post($slug){
         $post = Post::where('slug', $slug)->first();
         return view('web.post', compact('post'));
